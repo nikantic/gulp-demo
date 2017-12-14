f78e6081fec7bd73ffac9d8f90e882053c3bc025
Nikola Antić <nikantic@outlook.com>
2017-12-14 20:17:15 +0100

    add sass file and task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-	console.log("Hello world!");
+gulp.task('sass', function() {
+	return gulp.src('website/src/scss/styles.scss')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(gulp.dest('website/dist/css'));
 });
 
-gulp.task('default', ['hello']);
+gulp.task('default', ['sass']);
